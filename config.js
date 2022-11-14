fd9473713c70351360e467cd5f05cee999201273
Sreshta1234 <104977341+Sreshta1234@users.noreply.github.com>
2022-11-14 21:35:42 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDQb57enBNVsXUFfz6uezKk_SkHHK8UJwo",
+  authDomain: "story-telling-app-544cd.firebaseapp.com",
+  databaseURL: "https://story-telling-app-544cd-default-rtdb.firebaseio.com",
+  projectId: "story-telling-app-544cd",
+  storageBucket: "story-telling-app-544cd.appspot.com",
+  messagingSenderId: "316950438418",
+  appId: "1:316950438418:web:c279ef224a7bc11d79eecb"
+};
